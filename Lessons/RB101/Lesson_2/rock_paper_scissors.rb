67929c7cd17976bdde56433b600706baf77a6fb3
John Lee <john.cmlee@gmail.com>
2020-07-28 00:20:26 -0400

    Added score

--- Lessons/RB101/Lesson_2/rock_paper_scissors.rb
+++ Lessons/RB101/Lesson_2/rock_paper_scissors.rb
@@ -1,3 +1,7 @@
+# =====================
+# Constant Declarations
+# =====================
+
 # Choices
 # r = rock
 # p = paper
@@ -6,6 +10,22 @@
 # k = Spock
 VALID_CHOICES = ['r', 'p', 's', 'l', 'k']
 
+# Winning hash combinations
+WIN_HASH = {
+  'r' => ['l', 's'],
+  'p' => ['r', 'k'],
+  's' => ['p', 'l'],
+  'l' => ['k', 'p'],
+  'k' => ['s', 'r']
+}
+
+# Maximum Score
+MAX_SCORE = 5
+
+# ===================
+# Method Declarations
+# ===================
+
 # Prompt method
 def prompt(message)
   Kernel.puts("=> #{message}")
@@ -13,52 +33,72 @@ end
 
 # Win condition check
 def win?(first, second)
-  # Winning hash combinations
-  win_hash = {
-    'r' => ['l', 's'],
-    'p' => ['r', 'k'],
-    's' => ['p', 'l'],
-    'l' => ['k', 'p'],
-    'k' => ['s', 'r']
-  }
   # Checks if the second player's choice is included in the first player's value array
-  win_hash[first].include?(second)
+  WIN_HASH[first].include?(second)
 end
 
 # Display result method
 def display_result(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    prompt("You won this round!")
   elsif win?(computer, player)
-    prompt("The computer won!")
+    prompt("The computer won this round!")
   else
     prompt("It's a tie")
   end
 end
 
+# Update score
+def update_score(player, computer, score)
+  if win?(player, computer)
+    score[:player] += 1
+  elsif win?(computer, player)
+    score[:computer] += 1
+  end
+end
+
+# ==============
 # Main Game Loop
+# ==============
+
 loop do
+  # Variable Declarations
   player = '' # Initialize player choice
+  score = { player: 0, computer: 0 } # Initialize Scores
 
-  # Loop until valid choice
+  # Loop until one player has reached Max score value
   loop do
-    prompt("Choose one: [r]ock, [p]aper, [s]cissors, [l]izard, Spoc[k]")
-    player = Kernel.gets().chomp()
+    # Loop until valid user choice
+    loop do
+      prompt("Choose one: [r]ock, [p]aper, [s]cissors, [l]izard, Spoc[k]")
+      player = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(player)
-      break
-    else
-      prompt("That is not a valid choice")
+      if VALID_CHOICES.include?(player)
+        break
+      else
+        prompt("That is not a valid choice")
+      end
     end
+
+    computer = VALID_CHOICES.sample # Computer random choice
+    system("clear") # Clear screen
+
+    Kernel.puts("You chose: #{player}; Computer chose: #{computer}")
+    display_result(player, computer)
+    update_score(player, computer, score)
+    prompt("Current score is Player: #{score[:player]}; Computer #{score[:computer]}")
+    break if score.has_value?(MAX_SCORE)
   end
 
-  computer = VALID_CHOICES.sample
+  if score[:player] == MAX_SCORE
+    prompt("You have won!")
+  else
+    prompt("The computer has won!")
+  end
 
-  Kernel.puts("You chose: #{player}; Computer chose: #{computer}")
-  display_result(player, computer)
-
-  prompt("Do you want to play again?")
+  prompt("Do you want to play again? (y for yes, any other key to exit)")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?('y')
+  system("clear")
 end
 prompt("Thank you playing!")
