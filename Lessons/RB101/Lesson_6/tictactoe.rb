527ed4c3b1af9d63258270c14e6f28827a809589
John Lee <john.cmlee@gmail.com>
2020-08-16 21:56:05 -0400

    improved game AI

--- Lessons/RB101/Lesson_6/tictactoe.rb
+++ Lessons/RB101/Lesson_6/tictactoe.rb
@@ -3,6 +3,7 @@ require 'pry'
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+WIN_SCORE = 5
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
   [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
@@ -14,9 +15,11 @@ def prompt(msg)
 end
 
 # rubocop:disable Metrics/AbcSize
-def display_board(brd)
+def display_board(brd, points)
   system("clear")
-  puts "You're a #{PLAYER_MARKER}.  Computer is #{COMPUTER_MARKER}"
+  puts "First to #{WIN_SCORE} wins!"
+  puts "You're a #{PLAYER_MARKER}. Score: #{points[:player]}" \
+       "  Computer is #{COMPUTER_MARKER}. Score: #{points[:computer]}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -43,10 +46,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 def player_places_piece!(brd)
   square = ' '
   loop do
-    prompt "Choose a square (#{empty_squares(brd)}):"
+    prompt "Choose a square (#{joinor(empty_squares(brd))}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that is not a valid choice"
@@ -54,8 +68,40 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    # On the line combination that there is a win
+    # It will look for the remaining key that contains a blank
+    board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  end
+end
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+
+  # offense first
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
+
+  # defense
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, PLAYER_MARKER)
+      break if square
+    end
+  end
+
+  # just pick 5 or a square
+  if !square
+    square = if brd[5] == INITIAL_MARKER
+               5
+             else
+               empty_squares(brd).sample
+             end
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
@@ -69,45 +115,53 @@ end
 
 def detect_winner(brd)
   WINNING_LINES.each do |line|
-    # if brd[line[0]] == PLAYER_MARKER &&
-    #    brd[line[1]] == PLAYER_MARKER &&
-    #    brd[line[2]] == PLAYER_MARKER
-    #   return 'Player'
-    # elsif brd[line[0]] == COMPUTER_MARKER &&
-    #       brd[line[1]] == COMPUTER_MARKER &&
-    #       brd[line[2]] == COMPUTER_MARKER
-    #   return 'Computer'
-    # end
-    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
       return "Player"
-    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return "Computer"
     end
   end
   nil
 end
 
+# Main game loop
 loop do
-  board = initialize_board
-
+  score = { player: 0, computer: 0 }
+  # score loop
   loop do
-    display_board(board)
+    board = initialize_board
+    loop do
+      display_board(board, score)
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+      player_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
 
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+      computer_places_piece!(board)
+      break if someone_won?(board) || board_full?(board)
+    end
+
+    if someone_won?(board)
+      case detect_winner(board)
+      when "Player"
+        score[:player] += 1
+      when "Computer"
+        score[:computer] += 1
+      end
+      display_board(board, score)
+      prompt "#{detect_winner(board)} won!"
+    else
+      display_board(board, score)
+      prompt "It's a tie"
+    end
+    prompt "Press any key to continue"
+    gets.chomp
+
+    if score.value?(WIN_SCORE)
+      prompt "Final Score: Player: #{score[:player]}" \
+             " Computer: #{score[:computer]}"
+      break
+    end
   end
-
-  display_board(board)
-
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie"
-  end
-
   prompt "Play again? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
