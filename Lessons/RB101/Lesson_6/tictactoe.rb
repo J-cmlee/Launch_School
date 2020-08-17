b618869d300646b03c053cd0a0e334aa459fa45c
John Lee <john.cmlee@gmail.com>
2020-08-16 20:31:47 -0400

    refactored for Rubocop

--- Lessons/RB101/Lesson_6/tictactoe.rb
+++ Lessons/RB101/Lesson_6/tictactoe.rb
@@ -3,11 +3,17 @@ require 'pry'
 INITIAL_MARKER = " "
 PLAYER_MARKER = "X"
 COMPUTER_MARKER = "O"
+WINNING_LINES = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
+  [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
+  [1, 5, 9], [3, 5, 7] # diagonals
+]
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system("clear")
   puts "You're a #{PLAYER_MARKER}.  Computer is #{COMPUTER_MARKER}"
@@ -25,6 +31,7 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
@@ -61,20 +68,20 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines = [
-    [1, 2, 3], [4, 5, 6], [7, 8, 9], # rows
-    [1, 4, 7], [2, 5, 8], [3, 6, 9], # columns
-    [1, 5, 9], [3, 5, 7] # diagonals
-  ]
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-      return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
-      return 'Computer'
+  WINNING_LINES.each do |line|
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #    brd[line[1]] == PLAYER_MARKER &&
+    #    brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return "Player"
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+      return "Computer"
     end
   end
   nil
