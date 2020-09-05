abc804749eaf314a352800df737c4834f753bee7
J-cmlee <54999360+J-cmlee@users.noreply.github.com>
2020-09-05 15:24:56 -0400

    Delete easy4-palindomicnumbers\.rb

--- Exercises/02 - Ruby101-109/easy4-palindomicnumbers\.rb
+++
@@ -1,9 +0,0 @@
-def palindromic_number?(value)
-  check_string = value.to_s
-  check_string == check_string.reverse
-end
-
-p palindromic_number?(34543) == true
-p palindromic_number?(123210) == false
-p palindromic_number?(22) == true
-p palindromic_number?(5) == true
