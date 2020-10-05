1139b864a6ce5e0b9fb0129608a4b0c078fdbc3a
John Lee <john.cmlee@gmail.com>
2020-10-05 02:38:49 -0400

    modified Test

--- test.rb
+++ test.rb
@@ -1,19 +1,10 @@
-# frozen_string_literal: true
-
-def fizzbuzz(start_num, end_num)
-  output_array = []
-  (start_num..end_num).each do |ele|
-    if (ele % 5 == 0) && (ele % 3 == 0)
-      output_array << 'Fizzbuzz'
-    elsif (ele % 5) == 0
-      output_array << 'Buzz'
-    elsif ele % 3 == 0
-      output_array << 'Fizz'
-    else
-      output_array << ele
-    end
+def square_digits (num)
+  return_int = ""
+  digits_array = num.to_s.split("")
+  digits_array.each do |digit|
+    return_int << ((digit.to_i)**2).to_s
   end
-  output_array.join(", ")
+  return_int.to_i
 end
 
-p fizzbuzz(1, 15)
+p square_digits(9119)
