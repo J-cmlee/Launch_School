6467f25c6767dedee444cdca7a99e52a75496449
John Lee <john.cmlee@gmail.com>
2020-10-05 02:42:51 -0400

    update to merge

---
+++ test.rb
@@ -0,0 +1,11 @@
+def sequence(upperlimit)
+  return_array = []
+  1.upto(upperlimit) do |value|
+    return_array << value
+  end
+  return_array
+end
+
+p sequence(5) == [1, 2, 3, 4, 5]
+p sequence(3) == [1, 2, 3]
+p sequence(1) == [1]
